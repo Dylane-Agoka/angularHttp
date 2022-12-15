3a28d4d02cdfaf6d8d3a5f736d7be02a99850123
Dylane-Agoka <dylaneagoka47@gmail.com>
2022-12-15 09:52:45 +0100

    Clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,35 +14,7 @@ export class AppComponent implements OnInit {
     name: 'Junior Graham'
   };
 
-  constructor(private userService: UserService) {
-    // Understand Observables with RxJS
-    type HttpResponse = { code: number, data: string };
-
-    const observable = new Observable<HttpResponse>(subscriber => {
-      console.log('Inside subscriber');
-      subscriber.next({ code: 200, data: 'this is data 1...' });
-      subscriber.next({ code: 200, data: 'this is data 2...' });
-      subscriber.next({ code: 200, data: 'this is data 3...' });
-      // subscriber.error({ code: 500, message: 'An error occurred' });
-      setTimeout(() => {
-        subscriber.next({ code: 200, data: 'this is data more data...' });
-        subscriber.complete();
-      }, 3 * 1000);
-      console.log('subscriber is done emitting...');
-    });
-
-    observable.subscribe({
-      next(response: HttpResponse) {
-        console.log('got Response: ', response);
-      },
-      error(error: any) {
-        console.error('something wrong occurred: ', error);
-      },
-      complete() {
-        console.log('done');
-      }
-    });
-  }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.onGetUsers();
