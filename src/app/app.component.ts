2f9f11425ca4f5f731d33556c40ea82ea6529cfc
Dylane-Agoka <dylaneagoka47@gmail.com>
2022-12-15 09:52:05 +0100

    Understand observables with RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from './interfaces/user';
 import { UserService } from './services/user.service';
 
@@ -13,7 +14,35 @@ export class AppComponent implements OnInit {
     name: 'Junior Graham'
   };
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService) {
+    // Understand Observables with RxJS
+    type HttpResponse = { code: number, data: string };
+
+    const observable = new Observable<HttpResponse>(subscriber => {
+      console.log('Inside subscriber');
+      subscriber.next({ code: 200, data: 'this is data 1...' });
+      subscriber.next({ code: 200, data: 'this is data 2...' });
+      subscriber.next({ code: 200, data: 'this is data 3...' });
+      // subscriber.error({ code: 500, message: 'An error occurred' });
+      setTimeout(() => {
+        subscriber.next({ code: 200, data: 'this is data more data...' });
+        subscriber.complete();
+      }, 3 * 1000);
+      console.log('subscriber is done emitting...');
+    });
+
+    observable.subscribe({
+      next(response: HttpResponse) {
+        console.log('got Response: ', response);
+      },
+      error(error: any) {
+        console.error('something wrong occurred: ', error);
+      },
+      complete() {
+        console.log('done');
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.onGetUsers();
